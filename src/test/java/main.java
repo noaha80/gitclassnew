786e90065084151ee4e4291b3a8903ebc0711f0d
Noaha80 <noah.aboohamidi@gmail.com>
2026-02-01 16:25:45 -0600

    1stcommit

---
+++ src/test/java/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+        System.out.println("this is the project version 1");
+    }
+}
+//done with version one
