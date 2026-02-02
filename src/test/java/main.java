7e2d9214a7c66dec482e47939c968008b55d0778
Noaha80 <noah.aboohamidi@gmail.com>
2026-02-02 15:02:58 -0600

    3rdcommit

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -2,11 +2,9 @@ public class main {
     public static void main(String[] args) {
 
         System.out.println("hello world");
-        System.out.println("this is the project version 1");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
+        System.out.println("this is the project version 3");
+
 
     }
 }
-//done with version 2
+//done with version 3
