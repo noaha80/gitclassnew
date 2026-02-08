ef0a8923f263024f345931d3d61071680e0e048d
Noaha80 <noah.aboohamidi@gmail.com>
2026-02-08 11:37:33 -0600

    person2commit

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -2,9 +2,9 @@ public class main {
     public static void main(String[] args) {
 
 
-        System.out.println("this is the project version 5");
+        System.out.println("this is the project version 6 made by person 2");
 
 
     }
 }
-//done with version 4
+//done with version 6
